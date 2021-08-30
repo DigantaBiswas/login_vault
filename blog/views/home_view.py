164e61bedddd507a409bb87465a8d4e21970d470
Diganta <digantaprotik@gmail.com>
2021-08-30 15:05:52 +0600

    #added search functionality

--- blog/views/home_view.py
+++ blog/views/home_view.py
@@ -1,11 +1,11 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views import View
 
-from blog.models import Tag
+from blog.models import Tag, Post
 
 
 class HomeView(View):
-
     def get(self, request):
         tags = Tag.objects.filter()[:50]
         context = {
@@ -13,3 +13,13 @@ class HomeView(View):
             'title': "Home"
         }
         return render(request, 'base/login_vault_home.html', context)
+
+    def post(self, request):
+        search_text = request.POST.get("search_text")
+
+        posts = Post.objects.filter(Q(title__icontains=search_text) | Q(detail__icontains=search_text))
+
+        context = {
+            'posts':posts
+        }
+        return render(request, 'blog/post_list.html', context)
