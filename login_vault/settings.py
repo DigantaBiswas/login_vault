4a8e697a411f131ad411118e6c610c31b8ac025a
Diganta <digantaprotik@gmail.com>
2021-09-05 15:40:27 +0600

    #added postgres support for django app

--- login_vault/settings.py
+++ login_vault/settings.py
@@ -74,9 +74,18 @@ WSGI_APPLICATION = 'login_vault.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'loginvault',
+            'USER': 'postgres',
+            'PASSWORD': 'root',
+            'HOST': 'localhost',
+            'PORT': '5432',
     }
 }
 
