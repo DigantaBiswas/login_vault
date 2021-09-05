3badb497c30bfb9ce0f7ce54015c2af741e46b80
Diganta <digantaprotik@gmail.com>
2021-09-05 15:46:33 +0600

    #added mysql support for django app

--- login_vault/settings.py
+++ login_vault/settings.py
@@ -87,6 +87,15 @@ DATABASES = {
             'HOST': 'localhost',
             'PORT': '5432',
     }
+
+# 'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'DB_NAME',
+#         'USER': 'DB_USER',
+#         'PASSWORD': 'DB_PASSWORD',
+#         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
+#     }
 }
 
 # Password validation
